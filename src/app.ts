a377c1ef317c0c364ee42e14ea92568d617c4978
TomAbas <tructam37@gmail.com>
2022-11-26 08:10:02 +0700

    hafl http

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { readFileSync } from "fs";
 const http = require("http");
 
-const server = http.createServer((req: any, res: any) => {
-  console.log("123");
-  res.end("123");
-  return;
-});
+const homePage = readFileSync("src/index.html");
+const server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    const url = req.url;
+    if (url === "/") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(homePage);
+      res.end();
+    } else {
+      res.writeHead(404, { "content-type": "text/html" });
+      res.write("<h1>error</h1>");
+      res.end();
+    }
 
-server.listen("6868");
+    return;
+  }
+);
+
+server.listen("6868", () => {
+  console.log("aloalo");
+});
