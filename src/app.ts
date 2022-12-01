2ce966c563e450195b12fa1f97c7862297ae52d3
TomAbas <tructam37@gmail.com>
2022-12-01 08:25:40 +0700

    132

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express, { NextFunction, Request, Response } from "express";
 import { people, products } from "./data";
+import morgan from "morgan";
 import { logger } from "./middleware/logger";
 import { authorize } from "./middleware/authorize";
 const app = express();
 
 //req => middleware => res
-app.use([authorize, logger]);
+app.use(morgan("tiny"));
+// app.use(express.static("src/public"));
+// app.use([authorize, logger]);
+
 // app.use("/about", authorize);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("home");
+  res.send("<h1>home</h1>");
 });
 app.get("/about", (req: Request, res: Response) => {
   console.log(req.name);
